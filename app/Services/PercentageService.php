cc269a65e2a94b921f13a3af75ba854199b592f0
prince <nshutiprinceberyl@gmail.com>
2022-02-12 15:46:04 +0200

    created a percentageService to convert percentage and created a royaltyRewardService to perform its calculation

---
+++ app/Services/PercentageService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+
+
+class PercentageService
+{
+    /**
+     * In charge of converting two types of percentage form to float to be saved in the database
+     * @return float
+     */
+    public function convertToPercentage($value){
+        $percentage =$value;
+        if (str_contains((string)$value, '/')) {
+            $percentageParts = explode("/", $value);
+            $percentage = (float)((int)$percentageParts[0] / (int)$percentageParts[1]);
+        }
+        if (str_contains((string)$value, '%')) {
+            $percentageParts = explode("%", $value);
+            $percentage = (float)((int)$percentageParts[0] / 100);
+        }
+        return $percentage;
+    }
+}
